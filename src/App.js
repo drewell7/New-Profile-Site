bcac4043978c32b39568e2d18e4b6d7e8cdf7f40
drewell7 <91854055+drewell7@users.noreply.github.com>
2022-06-28 23:21:13 -0600

    Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Derek Ewell
-      </header>
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Header () {
+  return (
+    <div className='App-header'>
+      Derek Ewell
+    </div>
+  )
+}
+
+export default Header;
